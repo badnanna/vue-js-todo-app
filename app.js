377605c71e28ed6924fe66278a90ef41d79e9122
badre NANNA <badrenanna2020@gmail.com>
2024-08-10 00:50:43 +0100

    init repo, simple todo APP with vue js

---
+++ app.js
@@ -0,0 +1,29 @@
+const { createApp, ref } = Vue
+
+const app = createApp({
+  data() {
+    return {
+        todos: [],
+        todoInput: ''
+    }
+  },
+  methods : {
+    addTodo() {
+        if (this.todoInput !== ''){
+            this.todos.push({
+                id: this.todos.length + 1,
+                content: this.todoInput,
+                state: false
+            });
+        }
+        else {
+            alert ("please fill input first!")
+        }
+    },
+    changeState(index) {
+        this.todos[index].state = !this.todos[index].state
+    }
+  }
+})
+
+app.mount('#app')
